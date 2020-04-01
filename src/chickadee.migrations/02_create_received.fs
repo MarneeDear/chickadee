06883d1d548927a368e6b1d5bf1d500bf83aab9d
MarneeDear <>
2020-04-01 07:07:52 -0700

    Added migration scripts i missed in previous commit

---
+++ src/chickadee.migrations/02_create_received.fs
@@ -0,0 +1,17 @@
+﻿namespace chickadee.migrations
+open SimpleMigrations
+
+[<Migration(02L, "Create received")>]
+type CreateReceived() =
+  inherit Migration()
+
+  override __.Up() =
+    base.Execute(@"CREATE TABLE received (
+    	date_created	TEXT NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    	raw_packet	TEXT NOT NULL,
+    	packet_type	TEXT NOT NULL,
+    	error	TEXT
+    );")
+
+  override __.Down() =
+    base.Execute(@"DROP TABLE received;")
---
+++ src/chickadee.migrations/03_create_transmitted.fs
@@ -0,0 +1,18 @@
+﻿namespace chickadee.migrations
+open SimpleMigrations
+
+[<Migration(03L, "Create transmitted")>]
+type CreateTransmitted() =
+  inherit Migration()
+
+  override __.Up() =
+    base.Execute(@"CREATE TABLE transmitted (
+    	rowid	INTEGER NOT NULL,
+    	timestamp	TEXT NOT NULL,
+    	message	TEXT NOT NULL,
+    	type	TEXT NOT NULL,
+    	PRIMARY KEY(rowid)
+    );")
+
+  override __.Down() =
+    base.Execute(@"DROP TABLE transmitted;")
