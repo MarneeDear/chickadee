8841ca893a8baa3ee9754be4a62bcbc2022f6238
MarneeDear <>
2020-04-01 07:05:53 -0700

    Cahnge the MEssage DU and added RawPacket DU

--- src/chickadee.core/Message.fs
+++ src/chickadee.core/Message.fs
@@ -1,13 +1,37 @@
 ﻿namespace chickadee.core
 
+(*
+See APRS 101 14 MESSAGES, BULLETINS AND ANNOUNCEMENTS
+APRS messages, bulletins and announcements are packets containing free
+format text strings, and are intended to convey human-readable information.
+A message is intended for reception by a single specified recipient, and an
+acknowledgement is usually expected. Bulletins and announcements are
+intended for reception by multiple recipients, and are not acknowledged.
+Messages An APRS message is a text string with a specified addressee. The addressee
+is a fixed 9-character field (padded with spaces if necessary) following the :
+Data Type Identifier. The addressee field is followed by another :, then the
+text of the message.
+
+The message text may be up to 67 characters long, and may contain any
+printable ASCII characters except |, ~ or {.
+A message may also have an optional message identifier, which is appended
+to the message text. The message identifier consists of the character {
+followed by a message number (up to 5 alphanumeric characters, no spaces)
+to identify the message.
+
+Messages without a message identifier are not to be acknowledged.
+Messages with a message identifier are intended to be acknowledged by the
+addressee. The sending station will repeatedly send the message until it
+receives an acknowledgement, or it is canceled, or it times out.
+*)
 module Message =
 
     type MessageText = private MessageText of string
     module MessageText =
         let create (m:string) =
             match (m.Trim()) with
-            | m when m.Length <= 67    -> MessageText m //AX.25 field is 256 chars but the message has to accomodate the { for user defined messages
-            | _                         -> MessageText (m.Substring(0, 67)) //or return None TODO or throw an exception?
+            | m when m.Length <= 67    -> MessageText m 
+            | _                        -> MessageText (m.Substring(0, 67)) //TODO or return None TODO or throw an exception?
         let value (MessageText m) = m
 
     type MessageNumber = private MessageNumber of string
@@ -24,21 +48,24 @@ module Message =
             | : | Addressee | : | Message Text  | Message ID | Message Number (xxxxx)
     bytes   | 1 |    9      | 1 |    0-67       |     {      |      1-5
     :KG7SIO___:HELLO WORLD{1111
+    See APRS 101 14 MESSAGES, BULLETINS AND ANNOUNCEMENTS
     *)
     type Message =
         {
-            Addressee : CallSign
-            MessageText : MessageText
-            MessageNumber : MessageNumber
+            Addressee       : CallSign
+            MessageText     : MessageText
+            MessageNumber   : MessageNumber
         }
         override this.ToString() =
             sprintf ":%s:%s{%s" (CallSign.value this.Addressee) (MessageText.value this.MessageText) (MessageNumber.value this.MessageNumber)
 
+    //TODO
     type Bulletin = 
         {
             TODO : string
         }
 
+    //TODO
     type Announcement =
         {
             TODO2 : string
--- src/chickadee.core/TNC2MON.fs
+++ src/chickadee.core/TNC2MON.fs
@@ -35,6 +35,87 @@ With a position report
 module TNC2MON = 
     open Message
 
+    type RawInformation =
+        | CurrentMicEData
+        | OldMicEData
+        | PositionReportWithoutTimeStampOrUltimeter
+        | PeetBrosWeatherStation
+        | RawGPSDataOrUltimeter
+        | Argelo
+        | OldMicEButCurrentTMD700
+        | Item
+        | ShelterDataWithTime
+        | InvalidOrTest
+        | PositionReportWithTimestampNoMessaging
+        | Message 
+        | Object
+        | StationCapabilities
+        | PositionReportWithoutTimeStampWithMessaging
+        | StatusReport
+        | Query
+        | PositionReportWithTimestampWithMessaging
+        | TelemetryReport
+        | MaidenheadGridLocatorBeacon
+        | WeatherReportWihtoutPosition
+        | CurrentMicEDataNotUsedInTMD700
+        | UserDefined
+        | ThirdPartyTraffic
+        | Unsupported
+        override this.ToString() =
+            match this with
+            | CurrentMicEData                               -> "Current Mic-E Data"
+            | OldMicEData                                   -> "Old Mic-E Data"
+            | PositionReportWithoutTimeStampOrUltimeter     -> "Position Report without timestamp or Ultimeter station"
+            | PeetBrosWeatherStation                        -> "Peet Bros Weather Stations"
+            | RawGPSDataOrUltimeter                         -> "Raw GPS Data or Ultimeter station"
+            | Argelo                                        -> "Argelo DFJr/MicroFinder"
+            | OldMicEButCurrentTMD700                       -> "Old Mic-E Data (but Current data for TM-D700)"
+            | Item                                          -> "Item"
+            | ShelterDataWithTime                           -> "[Reserved — Shelter data with time]"
+            | InvalidOrTest                                 -> "Invalid or Test" 
+            | PositionReportWithTimestampNoMessaging        -> "Position with timestamp (no APRS messaging)"
+            | Message                                       -> "Message"
+            | Object                                        -> "Object"
+            | StationCapabilities                           -> "Station Capabilities"
+            | PositionReportWithoutTimeStampWithMessaging   -> "Position without timestamp (with APRS messaging)"
+            | StatusReport                                  -> "Status Report"
+            | Query                                         -> "Query"
+            | PositionReportWithTimestampWithMessaging       -> "Position with timestamp (with APRS messaging)"
+            | TelemetryReport                               -> "Telemetry Report"
+            | MaidenheadGridLocatorBeacon                   -> "Maidenhead grid locator beacon (obsolete)"
+            | WeatherReportWihtoutPosition                  -> "Weather Report (without position)"
+            | CurrentMicEDataNotUsedInTMD700                -> "Current Mic-E Data (not used in TM-D700)"
+            | UserDefined                                   -> "User Defined"
+            | ThirdPartyTraffic                             -> "Third-party traffic"
+            | Unsupported                                   -> "Unsupported"
+
+    let getRawPaketType id =
+        match id with
+        | "\u1c" -> CurrentMicEData
+        | "\u1d" -> OldMicEData
+        | "!" -> PositionReportWithoutTimeStampOrUltimeter
+        | "#" -> PeetBrosWeatherStation
+        | "$" -> RawGPSDataOrUltimeter
+        | "%" -> Argelo
+        | "'" -> OldMicEButCurrentTMD700
+        | ")" -> Item
+        | "*" -> PeetBrosWeatherStation
+        | "+" -> ShelterDataWithTime
+        | "," -> InvalidOrTest
+        | "/" -> PositionReportWithTimestampNoMessaging
+        | ":" -> Message
+        | ";" -> Object
+        | "<" -> StationCapabilities
+        | "=" -> PositionReportWithoutTimeStampWithMessaging
+        | ">" -> StatusReport
+        | "?" -> Query
+        | "@" -> PositionReportWithTimestampWithMessaging
+        | "T" -> TelemetryReport
+        | "[" -> MaidenheadGridLocatorBeacon
+        | "{" -> UserDefined
+        | "}" -> ThirdPartyTraffic        
+        | _   -> Unsupported
+
     type Information =
         | Message                           of MessageType
         | PositionReport                    of PositionReportType //PositionReportWithoutTimeStamp
