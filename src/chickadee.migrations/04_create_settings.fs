bc405b0e11ea662ba160105e2c869d62df879cdc
MarneeDear <>
2020-05-23 09:53:15 -0700

    Added a migration to create the settings table.

---
+++ src/chickadee.migrations/04_create_settings.fs
@@ -0,0 +1,17 @@
+﻿namespace chickadee.migrations
+open SimpleMigrations
+
+[<Migration(04L, "Create settings")>]
+type CreateSettings() =
+  inherit Migration()
+
+  override __.Up() =
+    base.Execute(@"CREATE TABLE settings (
+    	call_sign	TEXT,
+    	location_latitude	NUMERIC,
+    	location_longitude	NUMERIC,
+    	PRIMARY KEY(call_sign)
+    );")
+
+  override __.Down() =
+    base.Execute(@"DROP TABLE settings;")
