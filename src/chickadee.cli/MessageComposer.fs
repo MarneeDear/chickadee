cddeb5fde5606fc80b7884f1ba0df72977b3280e
MarneeDear <>
2020-05-25 10:38:02 -0700

    Fixed bug in message composer

--- src/chickadee.cli/MessageComposer.fs
+++ src/chickadee.cli/MessageComposer.fs
@@ -28,9 +28,9 @@ let composeMessage (msg:ParseResults<CustomMessageArguments>) : Message.Message 
     | None, None, None -> failwith (sprintf "%s %s %s" addr msgText msgNum)
 
 let composeMessagePacket (msg:ParseResults<CustomMessageArguments>) (sender:CallSign) (destination:CallSign) = 
-    let addr = msg.GetResult(CommandArguments.Addressee)
+    let addressee = msg.GetResult(CommandArguments.Addressee)
     let message = msg.GetResult(CommandArguments.Message)
-    let addressee = "ADDRESSEE cannot be empty and must be 1 - 9 characters."
+    let addrMsg = "ADDRESSEE cannot be empty and must be 1 - 9 characters."
     let msgText = "MESSAGE TEXT must be less than 68 characters and cannot contain | ~"
     let msgNum = "MESSAGE NUMBER must be less than 10"
     let information =
@@ -42,12 +42,12 @@ let composeMessagePacket (msg:ParseResults<CustomMessageArguments>) (sender:Call
                                     } |> Message.MessageFormat.Message 
                                     |> Information.Message
         | None, Some _, Some _ -> failwith addressee //TODO use a proper flow/pipeline with result type instead?
-        | None, None, Some _ -> failwith (sprintf "%s %s" addr msgText)
-        | None, Some _, None -> failwith (sprintf "%s %s" addr msgNum)
+        | None, None, Some _ -> failwith (sprintf "%s %s" addrMsg msgText)
+        | None, Some _, None -> failwith (sprintf "%s %s" addrMsg msgNum)
         | Some _, None, None -> failwith (sprintf "%s %s" msgText msgNum)
         | Some _, Some _, None -> failwith msgNum
         | Some _, None, Some _ -> failwith msgText
-        | None, None, None -> failwith (sprintf "%s %s %s" addr msgText msgNum)
+        | None, None, None -> failwith (sprintf "%s %s %s" addrMsg msgText msgNum)
 
     {
         Sender = sender
